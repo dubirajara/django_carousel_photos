c02d897229e2f41a523901ad4672e60dd9f60b15
Diego Ubirajara <diego.ubirajara@mail.com>
2016-11-21 22:36:01 +0100

    update settings

--- django_carousel/settings.py
+++ django_carousel/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'django_carousel.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(os.path.dirname(BASE_DIR), "templates")],
+        'DIRS': [os.path.join((BASE_DIR), "templates")],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -122,4 +122,4 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), "media")
+MEDIA_ROOT = os.path.join((BASE_DIR), "media")
