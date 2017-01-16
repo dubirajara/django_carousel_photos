0c3feaa0b33062a3283f033ac13456e1c741304f
Diego Ubirajara <diego.ubirajara@mail.com>
2017-01-16 18:20:08 +0100

    add tests

--- django_carousel/core/tests.py
+++ django_carousel/core/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
+from django.shortcuts import resolve_url as r
 
-# Create your tests here.
+
+class HomeTest(TestCase):
+
+    def setUp(self):
+        self.response = self.client.get(r('photo_carousel'))
+
+    def test_get(self):
+        """GET 'Home' must return status code 200"""
+        self.assertEqual(200, self.response.status_code)
+
+    def test_template(self):
+        """'Home' must use template index.html and base.html"""
+        self.assertTemplateUsed(self.response, 'photos.html')
