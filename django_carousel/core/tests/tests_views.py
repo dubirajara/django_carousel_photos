5e49b0ea2c135bd4a46f0f24763097e4fbafa7e1
Diego Ubirajara <diego.ubirajara@mail.com>
2017-02-27 19:06:18 +0100

    refactor tests

--- django_carousel/core/tests/tests_views.py
+++ django_carousel/core/tests/tests_views.py
@@ -2,7 +2,6 @@ from django.test import TestCase
 from django.shortcuts import resolve_url as r
 from model_mommy import mommy
 
-from django_carousel.core.models import Photo
 
 
 class HomeTest(TestCase):
@@ -38,29 +37,3 @@ class HomeTest(TestCase):
             with self.subTest():
                 self.assertContains(self.response, expected)
 
-
-class ModelTest(TestCase):
-    def setUp(self):
-        self.photo = mommy.make('Photo')
-
-    def test_create(self):
-        """Check models data create"""
-        self.assertTrue(Photo.objects.exists())
-
-    def test_title_can_be_blank(self):
-        """Check title fields can be blank"""
-        field = Photo._meta.get_field('title')
-        self.assertTrue(field.blank)
-
-    def test_description_can_be_blank(self):
-        """Check description fields can be blank"""
-        field = Photo._meta.get_field('description')
-        self.assertTrue(field.blank)
-
-    def test_ordering(self):
-        """Check ordering to show"""
-        self.assertListEqual(['timestamp'], Photo._meta.ordering)
-
-    def test_str(self):
-        """Check __str__ return title field"""
-        self.assertEqual(self.photo.title, str(self.photo))
