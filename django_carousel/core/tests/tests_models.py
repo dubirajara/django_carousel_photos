d4c2439a0c7b3887bd2e631de64a6291584465c6
Diego Ubirajara <diego.ubirajara@mail.com>
2017-05-07 09:24:14 +0200

    update test

--- django_carousel/core/tests/tests_models.py
+++ django_carousel/core/tests/tests_models.py
@@ -1,5 +1,8 @@
+from shutil import rmtree
+from django.conf import settings
 from django.test import TestCase
 from model_mommy import mommy
+
 from django_carousel.core.models import Photo
 
 
@@ -10,6 +13,9 @@ class ModelTest(TestCase):
     def test_create(self):
         """Check models data create"""
         self.assertTrue(Photo.objects.exists())
+
+    def tearDown(self):
+        rmtree(settings.MEDIA_ROOT, ignore_errors=True)
 
     def test_title_can_be_blank(self):
         """Check title fields can be blank"""
@@ -28,3 +34,5 @@ class ModelTest(TestCase):
     def test_str(self):
         """Check __str__ return title field"""
         self.assertEqual(self.photo.title, str(self.photo))
+
+
