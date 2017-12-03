c6f04467f0d030ebbe90ff91a6f9ec52300c0e99
Diego Ubirajara <diego.ubirajara@mail.com>
2017-12-03 08:54:33 +0100

    update django 2.0

--- django_carousel/urls.py
+++ django_carousel/urls.py
@@ -15,13 +15,13 @@ Including another URLconf
 """
 from django.conf import settings
 from django.conf.urls.static import static
-from django.conf.urls import url
+from django.urls import path
 from django.contrib import admin
 from .core import views
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'^$', views.photo_carousel, name='photo_carousel'),
+    path('admin/', admin.site.urls),
+    path('', views.photo_carousel, name='photo_carousel'),
 ]
 
 if settings.DEBUG:
